4f1643a19981505acdcadff791a5f00e06176de1
kmgraves22 <kgrave03@calpoly.edu>
2019-01-25 03:27:41 -0800

    Initial Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10;
+    public float jump = 10;
+
+    private float moveVelocity;
+    //private bool grounded = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(GetComponent<Rigidbody>().velocity.x, jump, 0);
+        }
+
+        moveVelocity = 0;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveVelocity = -speed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveVelocity = speed;
+        }
+        GetComponent<Rigidbody>().velocity = new Vector3(moveVelocity, GetComponent<Rigidbody>().velocity.y, 0);
+    }
+}
