6e3ebbd8ebba9505391c447ce60024c5b0aacf2a
kmgraves22 <kgrave03@calpoly.edu>
2019-01-31 04:56:02 -0800

    Restart Game Function
    restarts game when object is picked up

--- Assets/Scripts/PickUp.cs
+++ Assets/Scripts/PickUp.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PickUp : MonoBehaviour
 {
@@ -19,5 +20,7 @@ public class PickUp : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         this.gameObject.SetActive(false);
+        SceneManager.LoadScene("Prototype");
+
     }
 }
