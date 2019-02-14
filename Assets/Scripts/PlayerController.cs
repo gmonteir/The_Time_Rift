5b7665c0f4c1967725b3b158dfd058c9295a71ca
Nikita Jain <jainhnikita@gmail.com>
2019-02-14 14:59:42 -0800

    fixed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PrototypePlayerController : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     public float speed = 10;
     public float jump = 20;
