0ca6bf43a351008da29c751501637b09419026e8
kmgraves22 <kgrave03@calpoly.edu>
2019-02-25 14:43:42 -0800

    Progress Bar

---
+++ Assets/HealthController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthController : MonoBehaviour
+{
+    public ProgressBar pb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pb.BarValue = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pb.BarValue = pb.BarValue - 1;
+
+    }
+}
