dc30112775330e8746b6152b8feabc302c146c1b
kmgraves22 <kgrave03@calpoly.edu>
2019-02-27 03:28:04 -0800

    In Class Changes

--- Assets/HealthController.cs
+++ Assets/HealthController.cs
@@ -5,17 +5,31 @@ using UnityEngine;
 public class HealthController : MonoBehaviour
 {
     public ProgressBar pb;
+    public int bullet_damage;
 
     // Start is called before the first frame update
     void Start()
     {
         pb.BarValue = 100;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        pb.BarValue = pb.BarValue - 1;
+
 
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //If the player gets shot by an enemy bullet
+        print("got here"); 
+        if (collision.gameObject.tag == "Bullet")
+        {
+            pb.BarValue -= bullet_damage;
+            Destroy(collision);
+        }
+    }
+
 }
