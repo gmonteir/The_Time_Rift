70a52e2d5a6cb375079e9d51233a70488c90d059
Amy Valukonis <amyvalukonis@gmail.com>
2019-03-09 18:21:39 -0800

    Added intro cut scenes before tutorial

---
+++ Assets/Cutscene2Script.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement; 
+
+public class Cutscene2Script : MonoBehaviour
+{
+    public Text firstText;
+    public Text secondText;
+    public GameObject player; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        firstText.enabled = true;
+        firstText.text = "That is, until you somehow find yourself transported to the future right next to a time police facility";
+        secondText.enabled = false; 
+        StartCoroutine(DisplayText());
+    }
+
+    IEnumerator DisplayText()
+    {
+        yield return new WaitForSeconds(5);
+        firstText.enabled = false;
+        secondText.enabled = true;
+        Vector3 theScale = player.transform.localScale;
+        theScale.x *= -1;
+        player.transform.localScale = theScale;
+        secondText.text = "The solution goes like this: fix all the rifts in time and you get to go back home";
+        yield return new WaitForSeconds(5f);
+        SceneManager.LoadScene("Tutorial");
+    }
+}
---
+++ Assets/UIScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement; 
+
+public class UIScript : MonoBehaviour
+{
+    public Text firstText;
+    public Text secondText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //firstText = GetComponent<Text>();
+        //secondText = GetComponent<Text>();
+        firstText.enabled = true;
+        firstText.text = "You are a 30 - year - old police officer from modern day";
+        secondText.enabled = false; 
+        StartCoroutine(DisplayText()); 
+    }
+
+
+    IEnumerator DisplayText()
+    {
+        yield return new WaitForSeconds(5);
+        firstText.enabled = false;
+        secondText.enabled = true; 
+        secondText.text = "City crime is at an all - time low, and work life has been pretty stagnant";
+        yield return new WaitForSeconds(5);
+        SceneManager.LoadScene("TutorialCutscene2");
+    }
+}
